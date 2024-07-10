7f48775707f9aca6bdc4113a5ce762511712df9a
DiandraDaum <diandra.daumiller@gmail.com>
2024-07-10 14:01:31 +0200

    Code to convert Uniprot IDs into gene symbols for file Vento v5.

--- code/Modify_Vento.R
+++ code/Modify_Vento.R
@@ -2,58 +2,30 @@
 #Code to modify Human-2018-Vento-Tormo-LR-pairs.csv and 
 #interaction_input_Vento_Cellphonedbv5.csv to extract ligand and receptors gene symbols
 
+#BiocManager::install("org.Hs.eg.db")
+library('org.Hs.eg.db')
 # Read the .csv file
 #data <- read.csv("/Users/diandra/rlp_meta/data/Cellphone/Human-2018-Vento-Tormo-LR-pairs.csv")
 data <- read.csv("/Users/diandra/rlp_meta/data/Cellphone/interaction_input_Vento_Cellphonedbv5.csv", stringsAsFactors = FALSE)
 
-# Create the ligand column
-data$ligand <- ifelse(is.na(data$protein_name_a) | data$protein_name_a == "", 
-                      data$partner_a, 
-                      sub("_.*", "", data$protein_name_a))
+data$partner_a <- ifelse(is.na(mapIds(org.Hs.eg.db, keys=data$partner_a, column="SYMBOL", keytype="UNIPROT", multiVals="first")), 
+                         data$partner_a, 
+                         mapIds(org.Hs.eg.db, keys=data$partner_a, column="SYMBOL", keytype="UNIPROT", multiVals="first"))
 
-# Create the receptor column
-data$receptor <- ifelse(is.na(data$protein_name_b) | data$protein_name_b == "", 
-                        data$partner_b, 
-                        sub("_.*", "", data$protein_name_b))
+data$partner_b <- ifelse(is.na(mapIds(org.Hs.eg.db, keys=data$partner_b, column="SYMBOL", keytype="UNIPROT", multiVals="first")), 
+                         data$partner_b, 
+                         mapIds(org.Hs.eg.db, keys=data$partner_b, column="SYMBOL", keytype="UNIPROT", multiVals="first"))
 
-# Check for swapped values and swap them if necessary
-swapped_rows <- grepl("receptor", data$ligand, ignore.case = TRUE) | grepl("ligand", data$receptor, ignore.case = TRUE)
-swapped_rows[is.na(swapped_rows)] <- FALSE  # Replace NA with FALSE
-if (any(swapped_rows)) {
-  cat("Swapped values found. Swapping...\n")
-}
-data[swapped_rows, c("ligand", "receptor")] <- data[swapped_rows, c("receptor", "ligand")]
-
-# Write the updated data to a new .csv file
-#write.csv(data, "/Users/diandra/rlp_meta/data/new_files/Human-2018-Vento-Tormo-LR-pairs_new.csv", row.names = FALSE)
+# Write the modified data back to the same file
 write.csv(data, "/Users/diandra/rlp_meta/data/new_files/interaction_input_Vento_Cellphonedbv5_new.csv", row.names = FALSE)
 
 
 
+#create ligand and receptor columns
+data <- read.csv("/Users/diandra/rlp_meta/data/new_files/interaction_input_Vento_Cellphonedbv5_new.csv", stringsAsFactors = FALSE)
 
-#check vento again
-# Load the necessary libraries
-library(org.Hs.eg.db)
-library(readr)
+data$ligand <- data$partner_a
+data$receptor<- data$partner_b
+write.csv(data, "/Users/diandra/rlp_meta/data/new_files/interaction_input_Vento_Cellphonedbv5_new.csv", row.names = FALSE)
 
-# Load your data
-data <- read.csv("~/Downloads/DB/LewisLabUCSD/Human-2018-Vento-Tormo-LR-pairs.csv", stringsAsFactors = FALSE)
-
-#Extract the Uniprot ID part from the string
-data$partner_a_uniprot <- sub("_HUMAN", "", data$protein_name_a)
-data$partner_b_uniprot <- sub("_HUMAN", "", data$protein_name_b)
-
-# Convert Uniprot IDs to gene symbols using the uniprot package
-ids <- unique(c(data$partner_a_uniprot, data$partner_b_uniprot))
-conv <- id_mapping(ids, from = "UNIPROTKB", to = "GENENAME")
-
-# Merge the converted IDs back into the data
-data <- merge(data, conv, by.x = c("partner_a_uniprot", "partner_b_uniprot"), by.y = c("from", "from"))
-
-# Rename the columns
-data <- data %>% 
-  rename(partner_a_symbol = to, partner_b_symbol = to)
-
-# Save the updated data to the same file
-write.csv(data, "~/Downloads/DB/LewisLabUCSD/Human-2018-Vento-Tormo-LR-pairs_new.csv", row.names = FALSE)
 
