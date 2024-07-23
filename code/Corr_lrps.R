b7dfbe88e24db78f6273dfd2597a9ff02fda4c9c
DiandraDaum <diandra.daumiller@gmail.com>
2024-07-23 17:50:55 +0200

    NA handling in correlation issue solved.

--- code/Corr_lrps.R
+++ code/Corr_lrps.R
@@ -86,53 +86,57 @@ na_ri=length(rim_values)
 #spearman_corr <- cor.test(lim_values, rim_values, method = 'spearman')
 spearman_corrs <- matrix(NA, nrow(lim_values), 5)
 rownames(spearman_corrs) <- paste(rownames(lim_values), rownames(rim_values), sep = "_")
-colnames(spearman_corrs) <- c("Tot_correlation", "p-value", "Tot_sample_size", "X_correlation", "HC_correlation")
+colnames(spearman_corrs) <- c("Tot_correlation", "p-value", "Tot_sample_size", "X_correlation", "H_correlation")
 
 for (i in 1:nrow(lim_values)) {
-  for (j in 1:nrow(rim_values)) {
-    li_values <- lim_values[i, , drop = FALSE]
-    ri_values <- rim_values[j, , drop = FALSE]
-    li_non_na <- sum(!is.na(li_values))
-    ri_non_na <- sum(!is.na(ri_values))
-    ok <- li_non_na > 0 & ri_non_na > 0
-    ok_x <- complete.cases(li_values[, grep("^X", colnames(li_values))]) & complete.cases(ri_values[, grep("^X", colnames(ri_values))])
-    ok_h <- complete.cases(li_values[, grep("^H", colnames(li_values))]) & complete.cases(ri_values[, grep("^H", colnames(ri_values))])
-    if (ok) {
-      if (li_non_na == ri_non_na) {
-        corr_result <- cor.test(li_values, ri_values, method = "spearman")
-        spearman_corrs[i, 1] <- corr_result$estimate
-        spearman_corrs[i, 2] <- corr_result$p.value
-        spearman_corrs[i, 3] <- min(li_non_na, ri_non_na)  # count the minimum number of non-NA values
-      } else (pass) {
-        corr_result <- cor.test(li_values, ri_values, method = "spearman")
-        spearman_corrs[i, 1] <- corr_result$estimate
-        spearman_corrs[i, 2] <- corr_result$p.value
-        spearman_corrs[i, 3] <- min(li_non_na, ri_non_na)  # count the minimum number of non-NA values
-       
-    } else {
-      spearman_corrs[i, 1] <- NA
-      spearman_corrs[i, 2] <- NA
-      spearman_corrs[i, 3] <- 0
-    }
-    if (any(ok_x)) {
-      if (sum(!is.na(li_values[, grep("^X", colnames(li_values))])) > 1 && sum(!is.na(ri_values[, grep("^X", colnames(ri_values))])) > 1) {
-        spearman_corrs[i, 4] <- cor(li_values[, grep("^X", colnames(li_values))], ri_values[, grep("^X", colnames(ri_values))], method = "spearman", use = "pairwise.complete.obs")
-      } else {
-        spearman_corrs[i, 4] <- NA
-      }
-    } else {
-      spearman_corrs[i, 4] <- NA
-    }
-    if (any(ok_h)) {
-      if (sum(!is.na(li_values[, grep("^H", colnames(li_values))])) > 1 && sum(!is.na(ri_values[, grep("^H", colnames(ri_values))])) > 1) {
-        spearman_corrs[i, 5] <- cor(li_values[, grep("^H", colnames(li_values))], ri_values[, grep("^H", colnames(ri_values))], method = "spearman", use = "pairwise.complete.obs")
-      } else {
-        spearman_corrs[i, 5] <- NA
-      }
-    } else {
-      spearman_corrs[i, 5] <- NA
-    }
+  li_values <- lim_values[i,, drop = FALSE]
+  ri_values <- rim_values[i,, drop = FALSE]
+  
+  # Calculate total correlation
+  ok_tot_li <- !is.na(li_values)
+  ok_tot_ri <- !is.na(ri_values)
+  ok_tot <- ok_tot_li & ok_tot_ri
+  if (any(ok_tot)) {
+    corr_test <- cor.test(li_values[ok_tot], ri_values[ok_tot], method = "spearman")
+    spearman_corr <- corr_test$estimate
+    p_value <- corr_test$p.value
+    tot_sample_size <- sum(ok_tot)
+  } else {
+    spearman_corr <- NA
+    p_value <- NA
+    tot_sample_size <- 0
   }
+  
+  # Calculate correlation for columns starting with "X"
+  x_li_values <- li_values[, grep("^X", colnames(li_values))]
+  x_ri_values <- ri_values[, grep("^X", colnames(ri_values))]
+  ok_x_li <- !is.na(x_li_values)
+  ok_x_ri <- !is.na(x_ri_values)
+  ok_x <- ok_x_li & ok_x_ri
+  if (any(ok_x)) {
+    x_corr <- cor(x_li_values[ok_x], x_ri_values[ok_x], method = "spearman")
+  } else {
+    x_corr <- NA
+  }
+  
+  # Calculate correlation for columns starting with "H"
+  h_li_values <- li_values[, grep("^H", colnames(li_values))]
+  h_ri_values <- ri_values[, grep("^H", colnames(ri_values))]
+  ok_h_li <- !is.na(h_li_values)
+  ok_h_ri <- !is.na(h_ri_values)
+  ok_h <- ok_h_li & ok_h_ri
+  if (any(ok_h)) {
+    h_corr <- cor(h_li_values[ok_h], h_ri_values[ok_h], method = "spearman")
+  } else {
+    h_corr <- NA
+  }
+  
+  # Store the results
+  spearman_corrs[i, 1] <- spearman_corr
+  spearman_corrs[i, 2] <- p_value
+  spearman_corrs[i, 3] <- tot_sample_size
+  spearman_corrs[i, 4] <- x_corr
+  spearman_corrs[i, 5] <- h_corr
 }
 #PEARSON correlation
 #example: corr.one <- cor(data$x[data$category=="One"], data$y[data$category=="One"], method = 'pearson')
