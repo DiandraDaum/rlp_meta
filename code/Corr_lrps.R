db6e8f347a354f9db50a60dc392c7a55a6d9a437
DiandraDaum <diandra.daumiller@gmail.com>
2024-07-23 18:55:22 +0200

    it works! but plots to add

--- code/Corr_lrps.R
+++ code/Corr_lrps.R
@@ -6,6 +6,10 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(writexl)
+#for CI for spearman: install.packages("DescTools") library(DescTools)
+#install.packages("fmsb")
+library(fmsb)
+library(ggplot2)
 
 #read database
 database <- read_xlsx("~/rlp_meta/results/alldb_at_least_2_counts_ligand.xlsx")
@@ -84,9 +88,73 @@ na_ri=length(rim_values)
 #If association is monotonically decreasing then rs = -1.
 #It can be used when association is nonlinear.It can be applied for ordinal variables.
 #spearman_corr <- cor.test(lim_values, rim_values, method = 'spearman')
-spearman_corrs <- matrix(NA, nrow(lim_values), 5)
+spearman_corrs <- matrix(NA, nrow(lim_values), 7)
 rownames(spearman_corrs) <- paste(rownames(lim_values), rownames(rim_values), sep = "_")
-colnames(spearman_corrs) <- c("Tot_correlation", "p-value", "Tot_sample_size", "X_correlation", "H_correlation")
+colnames(spearman_corrs) <- c("Tot_correlation", "p-value", "CI_lower", "CI_upper", "Tot_sample_size", "X_correlation", "H_correlation")
+
+for (i in 1:nrow(lim_values)) {
+  li_values <- lim_values[i,, drop = FALSE]
+  ri_values <- rim_values[i,, drop = FALSE]
+  
+  # Calculate total correlation
+  ok_tot_li <-!is.na(li_values)
+  ok_tot_ri <-!is.na(ri_values)
+  ok_tot <- ok_tot_li & ok_tot_ri
+  if (any(ok_tot)) {
+    corr_test <- cor.test(li_values[ok_tot], ri_values[ok_tot], method = "spearman")
+    corr_test2 <- spearman.ci.sas(li_values[ok_tot], ri_values[ok_tot], adj.bias=TRUE, conf.level=0.95)
+    spearman_corr <- corr_test$estimate
+    p_value <- corr_test$p.value
+    tot_sample_size <- sum(ok_tot)
+    ci_lower <- corr_test2$rho.ll
+    ci_upper <- corr_test2$rho.ul
+  } else {
+    spearman_corr <- NA
+    p_value <- NA
+    tot_sample_size <- 0
+    ci_lower <- NA
+    ci_upper <- NA
+  }
+  
+  # Calculate correlation for columns starting with "X"
+  x_li_values <- li_values[, grep("^X", colnames(li_values))]
+  x_ri_values <- ri_values[, grep("^X", colnames(ri_values))]
+  ok_x_li <-!is.na(x_li_values)
+  ok_x_ri <-!is.na(x_ri_values)
+  ok_x <- ok_x_li & ok_x_ri
+  if (any(ok_x)) {
+    x_corr <- cor(x_li_values[ok_x], x_ri_values[ok_x], method = "spearman")
+  } else {
+    x_corr <- NA
+  }
+  
+  # Calculate correlation for columns starting with "H"
+  h_li_values <- li_values[, grep("^H", colnames(li_values))]
+  h_ri_values <- ri_values[, grep("^H", colnames(ri_values))]
+  ok_h_li <-!is.na(h_li_values)
+  ok_h_ri <-!is.na(h_ri_values)
+  ok_h <- ok_h_li & ok_h_ri
+  if (any(ok_h)) {
+    h_corr <- cor(h_li_values[ok_h], h_ri_values[ok_h], method = "spearman")
+  } else {
+    h_corr <- NA
+  }
+  
+  # Store the results
+  spearman_corrs[i, 1] <- spearman_corr
+  spearman_corrs[i, 2] <- p_value
+  spearman_corrs[i, 3] <- ci_lower
+  spearman_corrs[i, 4] <- ci_upper
+  spearman_corrs[i, 5] <- tot_sample_size
+  spearman_corrs[i, 6] <- x_corr
+  spearman_corrs[i, 7] <- h_corr
+}
+#PEARSON correlation
+#example: corr.one <- cor(data$x[data$category=="One"], data$y[data$category=="One"], method = 'pearson')
+#pearson_corr <- cor.test(lim, rim, method = 'pearson')
+pearson_corrs <- matrix(NA, nrow(lim_values), 8)
+rownames(pearson_corrs) <- paste(rownames(lim_values), rownames(rim_values), sep = "_")
+colnames(pearson_corrs) <- c("Tot_correlation", "p-value", "CI_lower", "CI_upper", "Degree of freedom", "Tot_sample_size", "X_correlation", "H_correlation")
 
 for (i in 1:nrow(lim_values)) {
   li_values <- lim_values[i,, drop = FALSE]
@@ -97,14 +165,21 @@ for (i in 1:nrow(lim_values)) {
   ok_tot_ri <- !is.na(ri_values)
   ok_tot <- ok_tot_li & ok_tot_ri
   if (any(ok_tot)) {
-    corr_test <- cor.test(li_values[ok_tot], ri_values[ok_tot], method = "spearman")
-    spearman_corr <- corr_test$estimate
+    corr_test <- cor.test(li_values[ok_tot], ri_values[ok_tot], method = "pearson")
+    pearson_corr <- corr_test$estimate
     p_value <- corr_test$p.value
     tot_sample_size <- sum(ok_tot)
+    ci <- corr_test$conf.int
+    ci_lower <- ci[1]
+    ci_upper <- ci[2]
+    df <- corr_test$parameter
   } else {
-    spearman_corr <- NA
+    pearson_corr <- NA
     p_value <- NA
     tot_sample_size <- 0
+    ci_lower <- NA
+    ci_upper <- NA
+    df <- NA
   }
   
   # Calculate correlation for columns starting with "X"
@@ -114,7 +189,7 @@ for (i in 1:nrow(lim_values)) {
   ok_x_ri <- !is.na(x_ri_values)
   ok_x <- ok_x_li & ok_x_ri
   if (any(ok_x)) {
-    x_corr <- cor(x_li_values[ok_x], x_ri_values[ok_x], method = "spearman")
+    x_corr <- cor(x_li_values[ok_x], x_ri_values[ok_x], method = "pearson")
   } else {
     x_corr <- NA
   }
@@ -126,52 +201,47 @@ for (i in 1:nrow(lim_values)) {
   ok_h_ri <- !is.na(h_ri_values)
   ok_h <- ok_h_li & ok_h_ri
   if (any(ok_h)) {
-    h_corr <- cor(h_li_values[ok_h], h_ri_values[ok_h], method = "spearman")
+    h_corr <- cor(h_li_values[ok_h], h_ri_values[ok_h], method = "pearson")
   } else {
     h_corr <- NA
   }
   
   # Store the results
-  spearman_corrs[i, 1] <- spearman_corr
-  spearman_corrs[i, 2] <- p_value
-  spearman_corrs[i, 3] <- tot_sample_size
-  spearman_corrs[i, 4] <- x_corr
-  spearman_corrs[i, 5] <- h_corr
+  pearson_corrs[i, 1] <- pearson_corr
+  pearson_corrs[i, 2] <- p_value
+  pearson_corrs[i, 3] <- ci_lower
+  pearson_corrs[i, 4] <- ci_upper
+  pearson_corrs[i, 5] <- df
+  pearson_corrs[i, 6] <- tot_sample_size
+  pearson_corrs[i, 7] <- x_corr
+  pearson_corrs[i, 8] <- h_corr
 }
-#PEARSON correlation
-#example: corr.one <- cor(data$x[data$category=="One"], data$y[data$category=="One"], method = 'pearson')
-#pearson_corr <- cor.test(lim, rim, method = 'pearson')
-pearson_corrs <- matrix(NA, nrow(lim_values), 2)
-rownames(pearson_corrs) <- paste(rownames(lim_values), rownames(rim_values), sep = "_")
-colnames(pearson_corrs) <- c("X_correlation", "H_correlation")
-
-for (i in 1:nrow(lim_values)) {
-  for (j in 1:nrow(rim_values)) {
-    li_values <- lim_values[i, , drop = FALSE]
-    ri_values <- rim_values[j, , drop = FALSE]
-    ok_x <- complete.cases(li_values[, grep("^X", colnames(li_values))]) & complete.cases(ri_values[, grep("^X", colnames(ri_values))])
-    ok_h <- complete.cases(li_values[, grep("^H", colnames(li_values))]) & complete.cases(ri_values[, grep("^H", colnames(ri_values))])
-    if (any(ok_x)) {
-      pearson_corrs[i, 1] <- cor(li_values[, grep("^X", colnames(li_values))][ok_x], ri_values[, grep("^X", colnames(ri_values))][ok_x], method = "pearson")
-    } else {
-      pearson_corrs[i, 1] <- NA
-    }
-    if (any(ok_h)) {
-      pearson_corrs[i, 2] <- cor(li_values[, grep("^H", colnames(li_values))][ok_h], ri_values[, grep("^H", colnames(ri_values))][ok_h], method = "pearson")
-    } else {
-      pearson_corrs[i, 2] <- NA
-    }
-  }
-}
-
-
-#extract results
-#cor.res$estimate|pi value|confidence intervals|degrees of freedom
-
 
   
 #Store as result list in the beginning
-spearman_results <- 
-pearson_results <- 
-  
+# Create spearman_results
+spearman_results <- data.frame(lrp = rownames(spearman_corrs), 
+                               spearman_corr = spearman_corrs[, 1], 
+                               p_value = spearman_corrs[, 2], 
+                               CI_lower = spearman_corrs[, 3], 
+                               CI_upper = spearman_corrs[, 4], 
+                               Tot_sample_size = spearman_corrs[, 5])
+rownames(spearman_results) <- 1:nrow(spearman_results)
 
+# Create pearson_results
+pearson_results <- data.frame(lrp = rownames(pearson_corrs), 
+                              pearson_corr = pearson_corrs[, 1], 
+                              p_value = pearson_corrs[, 2], 
+                              CI_lower = pearson_corrs[, 3], 
+                              CI_upper = pearson_corrs[, 4], 
+                              Degree_of_freedom = pearson_corrs[, 5], 
+                              Tot_sample_size = pearson_corrs[, 6])
+rownames(pearson_results) <- 1:nrow(pearson_results)
+
+
+
+#plot results:
+ggplot(spearman_results, aes(x = 1:nrow(spearman_results), y = spearman_corr, label = paste("n_li =", n_li, ", n_ri =", n_ri))) + 
+  geom_point() + 
+  labs(x = "Ligand-Receptor Pairs", y = "Spearman Correlation Coefficient") + 
+  theme_classic()
