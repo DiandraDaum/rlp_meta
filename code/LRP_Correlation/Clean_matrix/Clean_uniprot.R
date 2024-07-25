63b6cf4e66a73c08d2ac034f22a990a3d4ecdf50
DiandraDaum <diandra.daumiller@gmail.com>
2024-07-25 15:08:22 +0200

    added code to clean suvarna.

--- code/LRP_Correlation/Clean_matrix/Clean_uniprot.R
+++ code/LRP_Correlation/Clean_matrix/Clean_uniprot.R
@@ -18,25 +18,66 @@ m$Protein <- mapIds(org.Hs.eg.db, keys=m$Protein, column="SYMBOL", keytype="UNIP
 # Write the updated matrix to a new CSV file
 write.csv(m, file="~/covid_data/ms_covid19_and_controls/Clean_matrix/ahern_2022_matrix_new.csv", row.names=FALSE)
 
-#modify byeon_2022_matrix-------------------------------------------------------
+#modify biomart-------------------------------------------------------
 # Read the CSV file
 m <- read.csv("~/covid_data/ms_covid19_and_controls/byeon_2022_matrix.csv")
 colnames(m)[1] <- "Protein"
 # Convert the first column from Uniprot IDs to Ensembl gene names using org.Hs.eg.db
-m$Protein <- mapIds(org.Hs.eg.db, keys=m$Protein, column="SYMBOL", keytype="UNIPROT", multiVals="first")
+#m$Protein <- mapIds(org.Hs.eg.db, keys=m$Protein, column="SYMBOL", keytype="UNIPROT", multiVals="first")
 # Identify proteins that are still NA after the first conversion
 #na_proteins <- m$Protein[is.na(m$Protein)]
 # Create a Mart object
-#ensembl <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
-# List available filters (uncomment to check)
-filters <- listFilters(ensembl)
+mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
+mart <- useDataset(dataset="hsapiens_gene_ensembl", mart=mart)
+
 # Use biomaRt to convert the remaining NA proteins
-#proteins_converted <- getBM(filters=c(uniprot_swissprot=na_proteins), attributes=c("hgnc_symbol"), mart=ensembl)
+#proteins_converted <- getBM(attributes=c("hgnc_symbol","ensembl_gene_id","entrezgene_id"), filters="hgnc_symbol", mart=mart, values=na_proteins, uniqueRows=TRUE, bmHeader = T)
+m$Protein <- getBM(attributes=c("hgnc_symbol","ensembl_gene_id","entrezgene_id"), filters="hgnc_symbol", mart=mart, values=m$Protein, uniqueRows=TRUE, bmHeader = T)
 # Update the m$Protein column with the new conversions
 #m$Protein[is.na(m$Protein)] <- proteins_converted$hgnc_symbol[match(na_proteins, proteins_converted$uniprot_swissprot)]
 # Write the updated matrix to a new CSV file
 write.csv(m, file="~/covid_data/ms_covid19_and_controls/Clean_matrix/byeon_2022_matrix_gene_symbols.csv", row.names=FALSE)
 
+
+
+
+
+
+#modify suvarna-----------------------------------------------------------------
+# assume 'df' is your matrix
+df <- read.csv("~/covid_data/ms_covid19_and_controls/suvarna_2021_frontiers_in_physiology_matrix.csv")
+
+# define a function to clean the gene ids
+clean_gene_id <- function(gene_id) {
+  # if the value contains ';' or ':', return NA to remove the row
+  if (grepl(";|:", gene_id)) {
+    return(NA)
+  }
+  # if the value starts with 'CON__', extract the part after '_'
+  else if (grepl("^CON__", gene_id)) {
+    return(gsub("CON__", "", gene_id))
+  }
+  # if the value starts with 'p|', extract the part between '|' and '|'
+  else if (grepl("^sp\\|", gene_id)) {
+    return(gsub("sp\\|(.*?)\\|.*", "\\1", gene_id))
+  }
+  # if the value starts with 'REV__sp|', extract the part between '|' and '|'
+  else if (grepl("^REV__sp\\|", gene_id)) {
+    return(gsub("REV__sp\\|(.*?)\\|.*", "\\1", gene_id))
+  }
+  # otherwise, return the original value
+  else {
+    return(gene_id)
+  }
+}
+
+# apply the cleaning function to the first column
+df[, 1] <- sapply(df[, 1], clean_gene_id)
+# remove rows with NA values
+df <- df[!is.na(df[, 1]), ]
+colnames(df)[1] <- "Protein"
+# save the cleaned matrix
+write.csv(df, "~/covid_data/ms_covid19_and_controls/To_clean/suvarna_2021_frontiers_in_physiology_matrix_new.csv", row.names = FALSE)
 
 
 
